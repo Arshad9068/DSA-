d2831ee5061f71f3e4a7501d64f0b28387ad8222
Arshad81 <malikarshad62045@gmail.com>
2025-02-14 17:36:05 +0530

    coco eating banana's problem edited

--- src/HouseRobberyII.java
+++ src/HouseRobberyII.java
@@ -1,8 +1,26 @@
 public class HouseRobberyII {
+    public int rob1(int[] nums) {
+        int n = nums.length;
+        int curr1 = 0, curr2 = 0;
+        int prev1 = nums[0];
+//        2,3,5,
+
+        int min =Integer.MAX_VALUE;
+        int prev = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            curr1 = Math.max(prev1, prev);
+            min = Math.min(curr1,min);
+            prev1 = prev;
+            prev = curr1;
+        }
+        return min;
+    }
+
     public int rob(int[] nums) {
         int n= nums.length;
         int curr1=0,curr2=0;
         int prev1 = nums[0];
+
         int prev = Math.max(nums[0],nums[1]);
         for(int i=2;i<n-1;i++){
             curr1 = Math.max(nums[i]+prev1,prev);
@@ -21,9 +39,10 @@ public class HouseRobberyII {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {2,3,5,9};
         HouseRobberyII obj = new HouseRobberyII();
         int result = obj.rob(nums);
         System.out.println(result);
+        System.out.println(obj.rob1(nums));
     }
 }
--- src/KoKoEatingBanana.java
+++ src/KoKoEatingBanana.java
@@ -1,4 +1,33 @@
 public class KoKoEatingBanana {
+    public static int minEatingSpeed1(int[] piles, int h) {
+        int end =0,start=0;
+        for(int i=0;i<piles.length;i++){
+            start += piles[i];
+            end = Math.max(piles[i],end);
+        }
+        start /= h;
+        if(start<=0) start = 1;
+        int ans=0;
+        while(start<=end ){
+            int k=0;
+            int mid = start + (end-start)/2;
+            for(int i=0;i<piles.length;i++){
+                k+=piles[i]/mid;
+                if(piles[i]%mid!=0){
+                    k+=1;
+                }
+            }
+            if(k<=h){
+                ans=mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+
+        }
+
+        return ans;
+    }
 
         public static int minEatingSpeed(int[] piles, int h) {
             int start = 0 , end=0, mid,ans=0;
@@ -10,6 +39,7 @@ public class KoKoEatingBanana {
             if(start<=0){
                 start = 1;
             }
+//            3,6,7,11
             System.out.println(start + " " +end);
             while(start<=end){
                 int total_Hours = 0;
@@ -35,6 +65,9 @@ public class KoKoEatingBanana {
             int[] arr ={805306368,805306368,805306368};
             int n = 1000000000;
         System.out.println(minEatingSpeed(arr,n));
+        int[] piles = {3,6,7,11};
+        System.out.println(minEatingSpeed1(piles,8));
+
     }
 
 }
